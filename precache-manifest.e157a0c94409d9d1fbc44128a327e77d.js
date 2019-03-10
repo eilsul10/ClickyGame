e37e57a49c3dfa15273512379a153ad2e09b5cf4
Eileen S <eileen.j.sul@gmail.com>
2019-03-09 23:27:56 -0600

    Updates

---
+++ precache-manifest.e157a0c94409d9d1fbc44128a327e77d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.2c3a1b5a.js"
+  },
+  {
+    "revision": "0fc0acb54857b8a10e06",
+    "url": "/ClickyGame/static/js/main.0fc0acb5.chunk.js"
+  },
+  {
+    "revision": "aa3b5aed22f6e2223fda",
+    "url": "/ClickyGame/static/js/2.aa3b5aed.chunk.js"
+  },
+  {
+    "revision": "0fc0acb54857b8a10e06",
+    "url": "/ClickyGame/static/css/main.5de51964.chunk.css"
+  },
+  {
+    "revision": "7964d3f66293eb7c5f641208c5490057",
+    "url": "/ClickyGame/index.html"
+  }
+];
