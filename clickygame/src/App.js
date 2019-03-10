2964547e1b5341c205d8d734a412e90373f65b29
Eileen S <eileen.j.sul@gmail.com>
2019-03-09 21:43:06 -0600

    fixed navbar

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CharacterCard from "./components/CharacterCard";
-import Wrapper from "./components/Wrapper";
 import Footer from "./components/Footer";
 import Jumbotron from "./components/Jumbotron";
 import NavBar from "./components/NavBar";
@@ -10,7 +9,7 @@ class App extends Component {
 
   state = {
     characters,
-    clickedCharacters: [], 
+    clickedCharacters: [],
     score: 0,
     highScore: 0,
     status: ""
@@ -18,38 +17,52 @@ class App extends Component {
 
   clickImage = event => {
     const character = event.target.alt;
+    const characterClicked = this.state.clickedCharacters.indexOf(character) > -1;
 
-    if (this.state.clickedCharacters.indexOf(character) > -1) {
+    if (characterClicked) {
       this.setState({ clickedCharacters: [], score: 0, highScore: this.score, status:  "Game Over! Click to play again." });
+      
       for (let i = characters.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [characters[i], characters[j]] = [characters[j], characters[i]];
       }
       return characters;
-
     }
-    else if 
+    
+    else if
     (this.state.clickedCharacters.length === 12) {
       this.setState({highScore: 12, status: "You Won! Click to play again.", clickedCharacters: []});
       return;
     }
-  }
+
+    else {
+      this.setState.score ++
+    }
+
+
+  };
 
   render() {
     return (
-    <Wrapper>
-      <NavBar />
+    <div>
+      <NavBar
+        score = {this.state.score}
+        highScore={this.state.highScore}
+      />
       <Jumbotron />
+      <div className= "row">
       {this.state.characters.map(characters => (
         <CharacterCard
-            clickCharacter={this.clickCharacter}
+            clickImage={this.clickImage}
             id={characters.id}
             key={characters.id}
             image={characters.image}
         />
+
       ))}
+      </div>
       <Footer />
-    </Wrapper>
+      </div>
     );
   }
 }
--- clickygame/src/components/NavBar/index.js
+++ clickygame/src/components/NavBar/index.js
@@ -3,24 +3,16 @@ import React from "react";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            Bob's Burgers Clicky Game
-          </li>
-          <li className="nav-item">
-            Click an Image to Begin!
-          </li>
-          <li className="nav-item">
-            Score: {props.score}
-          </li>
-          <li className="nav-item">
-            High Score: {props.highScore}
-          </li>
+      <nav>
+        <div className="nav-wrapper">
+        <a href="#" class="brand-logo">Bob's Burgers Clicky Game</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li>Score: {props.score} &nbsp;</li>
+        
+        <li>High Score:  {props.highScore}</li>
         </ul>
-      </div>
-    </nav>
+       </div>
+      </nav>
   );
 }
 
